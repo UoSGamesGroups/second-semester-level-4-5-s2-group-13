a00fb217f1f538fdededb3654d48fb198d428ff8
Heidi Falcon <s180047@ucs.ac.uk>
2017-01-30 14:49:42 +0000

    Add files via upload

---
+++ Assets/Scripts/BubbleControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleControl : MonoBehaviour {
+
+    public float Size;
+
+
+	// Use this for initialization
+	void Start () {
+        Size = Random.Range(0, 1f);
+        transform.localScale = new Vector3(Size, Size, 0);
+        StartCoroutine(Timer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
